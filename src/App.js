e1645072bfb021989e25c7bb85a35dbdf37f48f0
“purplease-mohan” <mohan@purplease.com>
2025-04-30 08:30:19 -0400

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,82 @@
+import { useState } from "react";
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+
+//Database
+import products from "./db/data";
+import Card from "./components/Card";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  //Input Filter
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  //Radio Filter
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  //Buttons Filter
+
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const filteredData = (products, selected, query) => {
+    let filteredProducts = products;
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    //selected filtered
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ Category, color, company, newPrice, title }) =>
+          Category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, newPrice, prevPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          newPrice={newPrice}
+          prevPrice={prevPrice}
+        />
+      )
+    );
+  };
+
+  const result = filteredData(products, selectedCategory, query);
+
+  return (
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
+  );
 }
 
 export default App;
